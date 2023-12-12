12f4d5cad9d9615f50605cce17d1626c64297e91
Deokgyung <devduck@mailplug.co.kr>
2023-12-12 17:43:21 +0900

    feat-auth/feat: 아이콘 추가

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -4,5 +4,16 @@ import History from "./history.svg";
 import Myprofile from "./myprofile.svg";
 import Noti from "./noti.svg";
 import Walk from "./walk.svg";
+import DropDown from "./dropdown.svg";
+import PlusCircle from "./plus-circle.svg";
 
-export { Arrow, Calendar, History, Myprofile, Noti, Walk };
+export {
+  Arrow,
+  Calendar,
+  History,
+  Myprofile,
+  Noti,
+  Walk,
+  DropDown,
+  PlusCircle,
+};
