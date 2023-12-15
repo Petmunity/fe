f2ef8394edf98c105016996645a83657beaf67c7
Deokgyung <devduck@mailplug.co.kr>
2023-12-15 17:25:08 +0900

    feat-auth/fix: 불필요한 코드 삭제

--- src/components/DndButtonContainer.tsx
+++ src/components/DndButtonContainer.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState, useCallback } from "react";
 import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import DraggableBlockLinkButton from "./DraggableButton";
 import { TouchBackend } from "react-dnd-touch-backend";
 
@@ -41,45 +40,14 @@ export default function DndButtonContainer({
     [canDrag],
   );
 
-  //   const moveItem = async (dragIndex, hoverIndex) => {
-  //     setItems((prevItems) => {
-  //       const newItems = [...prevItems];
-  //       const draggedItem = newItems[dragIndex];
-
-  //       newItems.splice(dragIndex, 1);
-  //       newItems.splice(hoverIndex, 0, draggedItem);
-
-  //       return newItems;
-  //     });
-
-  //     // API 요청을 보내서 아이템의 위치를 저장
-  //     const response = await fetch("/api/save-item-position", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         itemId: items[dragIndex].id,
-  //         newPosition: hoverIndex,
-  //       }),
-  //     });
-
-  //     const data = await response.json();
-
-  //     if (!response.ok) {
-  //       throw new Error(data.message || "Failed to save item position");
-  //     }
-  //   };
-
-  console.log(items, "items");
   return (
     <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
       <div className="grid grid-cols-2 gap-4">
         {items.map((item, index) => (
           <DraggableBlockLinkButton
-            key={item.title}
-            id={item.title} // 고유한 아이디가 필요합니다.
-            index={index} // 드래그 앤 드롭을 위한 인덱스입니다.
+            key={item.title + index}
+            id={item.title}
+            index={index}
             src={item.src}
             title={item.title}
             href={item.href}
