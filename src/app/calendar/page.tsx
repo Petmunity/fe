506d497cdff8224997f63fb4e932cff7097f3f3a
Deokgyung <devduck@mailplug.co.kr>
2023-12-18 14:24:06 +0900

    feat-auth/feat: 캘린더 페이지 초기 퍼블리싱

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,7 +1,13 @@
+"use client";
 import Header from "@/components/common/Header";
 import Navbar from "@/components/common/Navbar";
+import Tab from "@/components/common/Tab";
+import { useState } from "react";
 
 export default function CalendarPage() {
+  const labels = ["캘린더", "리포트"];
+
+  const [focusedIndex, setFocusedIndex] = useState(0);
   //   const { data, error } = useQuery('calendar', () => {
   //     return fetch('http://localhost:3000/api/calendar').then((res) =>
   //       res.json()
@@ -19,7 +25,15 @@ export default function CalendarPage() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header title="캘린더" />
-      <h1>Calendar</h1>
+      <Tab
+        labels={labels}
+        focusedIndex={focusedIndex}
+        changeIndex={setFocusedIndex}
+      />
+      <main>
+        {focusedIndex === 0 && <h1>캘린더</h1>}
+        {focusedIndex === 1 && <h1>리포트</h1>}
+      </main>
       <Navbar />
     </div>
   );
