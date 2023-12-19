8f2da42aca24acc937a44ea8d76052f8612cea16
Deokgyung <devduck@mailplug.co.kr>
2023-12-19 14:35:22 +0900

    feat-walk/feat: 산책 시간 로직 훅

---
+++ src/hooks/useWalkTimer.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+const useWalkTimer = (start: boolean) => {
+  const [walkTime, setWalkTime] = useState(0);
+  const [intervalId, setIntervalId] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (start) {
+      const id = window.setInterval(() => {
+        setWalkTime((prevTime) => prevTime + 1);
+      }, 1000);
+      setIntervalId(id);
+    } else {
+      if (intervalId !== null) window.clearInterval(intervalId);
+      setIntervalId(null);
+      setWalkTime(0);
+    }
+
+    return () => {
+      if (intervalId !== null) window.clearInterval(intervalId);
+    };
+  }, [start]);
+
+  return walkTime;
+};
+
+export default useWalkTimer;
