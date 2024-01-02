6429eb30f1878b04d04ccdb2c559b786c2dd4c60
Deokgyung <devduck@mailplug.co.kr>
2024-01-02 16:46:08 +0900

    feat-walk/feat: 글쓰기 버튼 로컬스토리지 연동

--- src/components/dnd/DndButtonContainer.tsx
+++ src/components/dnd/DndButtonContainer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 "use client";
 import { useState, useCallback } from "react";
 import { DndProvider } from "react-dnd";
@@ -5,19 +6,29 @@ import DndButton from "./DndButton";
 import { TouchBackend } from "react-dnd-touch-backend";
 import CustomDragLayer from "./CustomDragyer";
 
+type buttonItemsType = {
+  src: string;
+  title: string;
+  href: string;
+  bgColor: string;
+};
+
 interface DndButtonContainerProps {
-  buttonItems: {
-    src: string;
-    title: string;
-    href: string;
-    bgColor: string;
-  }[];
+  buttonItems: buttonItemsType[];
 }
 
 export default function DndButtonContainer({
   buttonItems,
 }: DndButtonContainerProps) {
-  const [items, setItems] = useState(buttonItems);
+  const loadItemsFromLocalStorage = () => {
+    const savedItemsString = localStorage.getItem("buttonItems");
+    if (savedItemsString) {
+      return JSON.parse(savedItemsString);
+    }
+    return buttonItems;
+  };
+
+  const [items, setItems] = useState(loadItemsFromLocalStorage);
   const [canDrag, setCanDrag] = useState(false);
 
   const toggleCanDrag = () => {
@@ -27,12 +38,15 @@ export default function DndButtonContainer({
   const moveItem = useCallback(
     (dragIndex: number, hoverIndex: number) => {
       if (canDrag) {
-        setItems((items) => {
+        setItems((items: buttonItemsType[]) => {
           const newItems = [...items];
           const draggedItem = newItems[dragIndex];
 
           newItems.splice(dragIndex, 1);
           newItems.splice(hoverIndex, 0, draggedItem);
+
+          // 로컬 스토리지에 아이템의 순서를 저장하기
+          localStorage.setItem("buttonItems", JSON.stringify(newItems));
 
           return newItems;
         });
@@ -49,7 +63,7 @@ export default function DndButtonContainer({
             canDrag && "motion-safe:animate-pulse"
           }`}
         >
-          {items.map((item, index) => (
+          {items.map((item: buttonItemsType, index: number) => (
             <DndButton
               key={item.title + index}
               id={item.title}
