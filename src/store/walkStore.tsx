feb12e2630e964b65130ad66881039b57781b707
Deokgyung <devduck@mailplug.co.kr>
2024-01-04 17:16:16 +0900

    main/feat: startPosition store add

--- src/store/walkStore.tsx
+++ src/store/walkStore.tsx
@@ -7,6 +7,7 @@ type Position = {
 };
 interface WalkStore {
   center: Position;
+  startPosition: Position;
   endPosition: Position;
   startTime: Date | null;
   endTime: Date | null;
@@ -16,6 +17,7 @@ interface WalkStore {
   setStartTime: (startTime: Date | null) => void;
   setEndTime: (endTime: Date | null) => void;
   setCenter: (center: Position) => void;
+  setStartPosition: (startPosition: Position) => void;
   setEndPosition: (endPosition: Position) => void;
   setTotalTime: (totalTime: number) => void;
   setPath: (pathUpdateFunction: (prevPath: Position[]) => Position[]) => void;
@@ -26,6 +28,7 @@ interface WalkStore {
 
 export const useWalkStore = create<WalkStore>((set) => ({
   center: { lat: 0, lng: 0 },
+  startPosition: { lat: 0, lng: 0 },
   endPosition: { lat: 0, lng: 0 },
   startTime: null,
   endTime: null,
@@ -35,6 +38,7 @@ export const useWalkStore = create<WalkStore>((set) => ({
   setStartTime: (startTime: Date | null) => set({ startTime }),
   setEndTime: (endTime: Date | null) => set({ endTime }),
   setCenter: (center: Position) => set({ center }),
+  setStartPosition: (startPosition: Position) => set({ startPosition }),
   setEndPosition: (endPosition: Position) => set({ endPosition }),
   // setTotalDistance: (
   //   distanceUpdateFunction: (prevDistance: number) => number,
